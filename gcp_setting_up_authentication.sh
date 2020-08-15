2e6b016c99a8dcd7dc4f5f1b7a3b214ce93cbddf
manorzvi <manor.zvi@campus.technion.ac.il>
2020-08-15 10:13:07 +0300

    comment export GOOGLE_APPLICATION_CREDENTIALS. in local machine: specify key-file explicitly

--- gcp_setting_up_authentication.sh
+++ gcp_setting_up_authentication.sh
@@ -21,7 +21,7 @@ PROJECT_ID=`gcloud config get-value project`
 echo "PROJECT_ID: $PROJECT_ID"
 
 echo "Create the service account ..."
-X=`gcloud iam service-accounts create $NAME`
+`gcloud iam service-accounts create $NAME`
 echo "Done."
 
 echo "Grant permissions to the service account ..."
@@ -32,8 +32,8 @@ echo "Generate the key file ..."
 gcloud iam service-accounts keys create $NAME.json --iam-account $NAME@$PROJECT_ID.iam.gserviceaccount.com
 echo "Done. "
 
-echo "Setting GOOGLE_APPLICATION_CREDENTIALS=$NAME.json env variable ..."
-export GOOGLE_APPLICATION_CREDENTIALS="$NAME.json"
-echo "Done."
+#echo "Setting GOOGLE_APPLICATION_CREDENTIALS=$NAME.json env variable ..."
+#export GOOGLE_APPLICATION_CREDENTIALS="$NAME.json"
+#echo "Done."
 
 
